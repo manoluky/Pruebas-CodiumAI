6f4bb301117674b9c6b50967c6ec00ddd9054382
manoluky <46540583+manoluky@users.noreply.github.com>
2024-02-19 15:20:44 -0300

    se agregan pruebas a RutTest

--- src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
+++ src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
@@ -212,37 +212,5 @@ class RutTest {
         // Act & Assert
         assertThrows(IllegalArgumentException.class, () -> converter.convertToEntityAttribute(rutString));
     }
-    @Test
-    public void test_isValid_with_invalid_number_and_digit() {
-        long number = -1;
-        char digit = 'A';
 
-        boolean result = Rut.isValid(number, digit);
-
-        assertFalse(result);
-    }
-
-    @Test
-    public void test_isValid_with_number_greater_than_99999999() {
-        Rut rut = new Rut(100000000, '0');
-
-        boolean result = rut.isValid();
-
-        assertFalse(result);
-    }
-    @Test
-    public void test_isValid_with_number_less_than_or_equal_to_0() {
-        Rut rut = new Rut(0, 'K');
-
-        boolean result = rut.isValid();
-
-        assertFalse(result);
-    }
-
-    @Test
-    public void test_valid_rut_creation() {
-        Rut rut = new Rut(12345678, 'K');
-        assertEquals(12345678, rut.getNumber());
-        assertEquals('K', rut.getDigit());
-    }
 }
